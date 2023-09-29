ad5c426da6841a17278b68bd3343b35f1f8df6e0
kalyan kumar <kalyan kumar@kalyan>
2023-09-29 10:05:58 +0530

    frist commit

---
+++ VariablesExample/src/VariablesEampless.java
@@ -0,0 +1,21 @@
+
+public class VariablesEampless {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		        int age = 30;  // Integer variable
+		        double height = 5.8;  // Double variable
+		        char grade = 'A';  // Character variable
+		        String name = "John Doe";  // String variable
+
+		        System.out.println("Age: " + age);
+		        System.out.println("Height: " + height);
+		        System.out.println("Grade: " + grade);
+		        System.out.println("Name: " + name);
+		    }
+		}
+
+
+
