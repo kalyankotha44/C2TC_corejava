d5103f684b48404de8e2996f3e439f3c615474b2
kalyan kumar <kalyan kumar@kalyan>
2023-10-08 20:16:55 +0530

    frist commit

---
+++ GetterSetter/src/StudentData.java
@@ -0,0 +1,13 @@
+
+public class StudentData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Student student =new Student();
+ student.setName("kalyan");
+ student.setAge(20);
+ 
+ System.out.println("Student:" +student.getName()+"Age:" + student.getAge());
+	}
+
+}
